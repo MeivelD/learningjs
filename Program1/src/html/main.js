93ef4a5b71f807470d17f287597450532b3ae57f
mdmeivel@gmail.com <mdmeivel@gmail.com>
2020-04-10 16:18:54 +0530

    AC-3: added register form and view details form

--- Program1/src/html/main.js
+++ Program1/src/html/main.js
@@ -1,22 +1,35 @@
 function validateForm() {
     var name = document.forms["registerform"]["name"].value;
-    /*  if (x == "") {
-         return false;
-     } */
     var psNo = document.forms["registerform"]["ps_no"].value;
     var dob = document.forms["registerform"]["dob"].value;
     var email = document.forms["registerform"]["email"].value;
     var dateOfFilling = document.forms["registerform"]["date"].value;
-
-    function myFunction() {
-        /* var today = new Date();
-        var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-        return date; */
-        alert(1);
-        document.getElementById("myDate").defaultValue = "2014-02-09";
-    }
-
     var gender = document.forms["registerform"]["gender"].value;
     var designation = document.forms["registerform"]["designation"].value;
     var experience = document.forms["registerform"]["experience"].value;
 }
+
+function setDefaultValues() {
+    var today = new Date();
+    var month = today.getMonth() + 1;
+    if (month < 10) {
+        month = "0" + month;
+    }
+    var date = today.getFullYear() + '-' + month + '-' + today.getDate();
+    //alert(date);
+
+    document.getElementById("myDate").defaultValue = date;
+
+}
+
+//Just For Reeference
+/* function resetFields() {
+    document.forms["registerform"]["name"].value = "";
+    document.forms["registerform"]["ps_no"].value = "";
+    document.forms["registerform"]["dob"].value = "";
+    document.forms["registerform"]["email"].value = "";
+    document.forms["registerform"]["date"].value = "";
+    document.forms["registerform"]["gender"].value = "";
+    document.forms["registerform"]["designation"].value = "";
+    document.forms["registerform"]["experience"].value = "";
+} */
